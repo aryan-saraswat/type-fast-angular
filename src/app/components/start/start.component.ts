1ca58838698ec02f21dc26a98cad935c15af02c2
Aryan Saraswat <aryan.saraswat@hotmail.co.uk>
2024-06-23 15:20:18 +0200

    style changes

--- src/app/components/start/start.component.ts
+++ src/app/components/start/start.component.ts
@@ -4,6 +4,7 @@ import { Router } from '@angular/router';
 import { ButtonModule } from 'primeng/button';
 import { CardModule } from 'primeng/card';
 import { InputNumberModule } from 'primeng/inputnumber';
+import { ListboxModule } from 'primeng/listbox';
 import { WordsService } from '../../services/words-service';
 
 @Component({
@@ -15,17 +16,37 @@ import { WordsService } from '../../services/words-service';
     InputNumberModule,
     CardModule,
     ButtonModule,
+    ListboxModule,
   ],
   templateUrl: './start.component.html',
   styleUrl: './start.component.scss',
 })
 export class StartComponent {
   displayedWords: number = 5;
+  selectedTime = 60;
+  times = [
+    {
+      label: '60 seconds',
+      value: 60,
+    },
+    {
+      label: '90 seconds',
+      value: 90,
+    },
+    {
+      label: '120 seconds',
+      value: 120,
+    },
+  ];
 
   constructor(private router: Router, private wordsService: WordsService) {}
 
   onSubmitClick() {
     this.wordsService.numberOfWords = this.displayedWords;
     this.router.navigate(['game']);
+  }
+
+  onSelectTime() {
+    console.log('selected time: ', this.selectedTime);
   }
 }
