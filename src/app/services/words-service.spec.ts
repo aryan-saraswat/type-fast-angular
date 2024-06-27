7e33bffdc45b5d5a9e96a8beef1c59698e86756a
Aryan Saraswat <aryan.saraswat@hotmail.co.uk>
2024-06-27 23:10:54 +0200

    incomplete test

--- src/app/services/words-service.spec.ts
+++ src/app/services/words-service.spec.ts
@@ -1,16 +1,37 @@
 import { TestBed } from '@angular/core/testing';
 
+import {
+  HttpTestingController,
+  provideHttpClientTesting,
+} from '@angular/common/http/testing';
 import { WordsService } from './words-service';
 
 describe('WordsServiceService', () => {
   let service: WordsService;
+  let httpMock: HttpTestingController;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      providers: [WordsService, provideHttpClientTesting()],
+    });
     service = TestBed.inject(WordsService);
+    httpMock = TestBed.inject(HttpTestingController);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('should get words from API', () => {
+    //TODO finish tests
+    const mockedWords = ['a', 'b', 'c', 'd', 'e'];
+    service.getWords().subscribe((words) => {
+      expect(words).toEqual(mockedWords);
+    });
+
+    const req = httpMock.expectOne(`${service.api}/get-words`);
+    expect(req.request.method).toBe('POST');
+    expect(req.request.body).toEqual({ numberOfWords: service.numberOfWords });
+    req.flush(mockedWords);
   });
 });
